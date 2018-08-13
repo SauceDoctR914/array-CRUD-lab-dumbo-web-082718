79c2e0c9f1706e216a407e3ea8fbec581b3dabc9
Gavin Soko <sokolofg@gmail.com>
2018-08-13 00:28:01 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,19 +8,23 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
+  array
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.shift(element)
+  array
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop()
+  array
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.unshift()
+  array
 end
 
 def retrieve_element_from_index(array, index_number)
